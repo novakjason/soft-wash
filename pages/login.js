e1dbf5a6792d2ddd9deba32c52a52c76ffe79c4d
Jason Novak <novak.jason7@gmail.com>
2019-10-05 13:41:52 -0400

    Add SignIn component and login page

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+import Layout from '../components/MyLayout';
+import SignIn from '../components/SignIn';
+
+export default function LogIn() {
+    return (
+        <Layout>
+            <SignIn />
+        </Layout>
+    )
+}
