a4cfd7ebc90c5c1be92604845e53651a64a9f546
Cohen <dcoco91@gmail.com>
2019-10-07 15:09:48 -0400

    added a CardPic component, added an icon to render later in the cards, added a static folder to serve images

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,35 @@
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/MyLayout.js';
+import CardPic from '../components/Card';
+import { makeStyles } from '@material-ui/core/styles';
+import HouseOutlinedIcon from '@material-ui/icons/HouseOutlined';
+
+const useStyles = makeStyles(theme => ({
+    cardbox: {
+      display: 'flex',
+      alignItems: 'center',
+    }
+}));
 
 export default function About() {
+
+  const classes = useStyles();
   return (
+   
     <Layout>
+   
       <p>This is the about page</p>
+      <div className={classes.cardbox}>
+        <HouseOutlinedIcon />
+      <CardPic 
+        title="Clean House"
+        about="This house, is clean. And it is pretty cause I washed it, dog"
+        />
+        <CardPic 
+        title="Clean House"
+        about="This house, is clean. And it is pretty cause I washed it, dog"
+        />
+      </div>
+     
     </Layout>
   )
 }
