4c769470f8862cd9d7ba00c6fe298fd13299adaf
Cohen <dcoco91@gmail.com>
2019-10-10 21:16:59 -0400

    added express server and next app, you can run the app with node server/app. added some packages to facilitate db connections. created User schema. created a HOC called withAuth that will wrap pages that need to be protected from non logged in users

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const next = require("next");
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+const dev = process.env.NODE_ENV !== `production`;
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+};
+
+mongoose.connect(MONGO_URL, options)
 
 const PORT = process.env.PORT || 3000
 const ROOT_URL = `http://localhost:${PORT}`
 
-// 
-const dev = process.env.NODE_ENV !== `production`;
 const app = next({ dev });
 const handle = app.getRequestHandler(); 
 
