d77a594d1a8ae080259ef88b8f948cf391d980a9
Cohen <dcoco91@gmail.com>
2019-10-10 21:31:53 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,8 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta
             name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+           />
           {/* PWA primary color */}
 
           <link 
@@ -19,11 +20,12 @@ class MyDocument extends Document {
             rel="stylesheet" 
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
             crossorigin="anonymous"
-         />
+          />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+           
         </Head>
         <body>
           <Main />
